e3c5b9fc7d93ee8e86c78d26a6437207f2dda2f1
Ömer Faruk Aksak <u0160661@kuleuven.be>
2024-03-08 10:37:16 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const arr = new Array(350).fill(0).map(x => Math.floor((Math.random() * 200 + 1)))
+const sleep = () => new Promise(res => {
+  setTimeout(() => {
+    res()
+  }, 0)
+})
+function swap(arr, firstItemIndex, SecondItemIndex) {
+  let temp = arr[firstItemIndex]
+  arr[firstItemIndex] = arr[SecondItemIndex]
+  arr[SecondItemIndex] = temp
+}
 function App() {
+  const [lines, setLines] = useState(arr)
+  useEffect(() => {
+    sort()
+  }, [])
+  const sort = async () => {
+    let count=0
+    for (let i = 0; i < lines.length; i++) {
+      for (let j = lines.length - 1; j > i; j--) {
+        count++;
+        if (lines[j] < lines[j - 1]) {
+          swap(lines, j, j - 1)
+          setLines([...lines])
+          await sleep()
+        }
+      }
+    }
+    console.log(count)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ height: `99vh`, width: `${lines.length * 5}px`, margin: 'auto', display: 'flex', alignItems: 'end' }}>
+        {
+          lines.map((l, index) => {
+            return (<div key={index} style={{ height: `${l}px`, width: '20px', backgroundColor: 'red' }}></div>)
+          })
+        }
+      </div>
     </div>
   );
 }
